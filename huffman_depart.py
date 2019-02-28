d77b74b4dec8a9c0511099c78b00a96e31eb97e4
mickael renaud <mickael.renaud@u-psud.fr>
2019-02-28 14:43:10 +0100

    tp de départ ajouté

---
+++ huffman_depart.py
@@ -0,0 +1,55 @@
+class Arbre:
+    def __init__(self, frequence, gauche, droit):
+        """ Construit un Arbre
+
+            frequence: int
+            gauche, droit: Arbre
+        """
+        self.frequence = frequence
+        self.gauche = gauche
+        self.droit = droit
+
+    def affiche(self, prefixes = ['    ']):
+        """ Affiche l'arbre """
+        print(''.join(prefixes[:-1]) + '|___' + str(self.frequence))
+        prefixes.append('|   ')
+        self.gauche.affiche(prefixes)
+        prefixes.pop()
+        prefixes.append('    ')
+        self.droit.affiche(prefixes)
+        prefixes.pop()
+
+class Feuille(Arbre):
+    def __init__(self, frequence, symbole):
+        """ Construit une feuille
+
+            frequence: int
+            symbole: str
+        """
+        Arbre.__init__(self, frequence, None, None)
+        self.symbole = symbole
+
+    def affiche(self, prefixes = ['    ']):
+        """ Affiche la feuille """
+        print("".join(prefixes[:-1]) + '|___' +
+                str(self.frequence) +
+                '(' + self.symbole + ')')
+
+class Huffman:
+    """ Algorithme de construction de l'arbre de Huffman """
+    def __init__(self, frequences):
+        """ Constructeur
+
+            frequences: dictionnaire des fréquences
+        """
+        self.foret = []
+
+if __name__ == "__main__":
+    A = Arbre(18,
+              Arbre(8,
+                    Arbre(3,
+                          Feuille(1, 'd'),
+                          Feuille(2, 'c')),
+                    Feuille(5, 'b')),
+              Feuille(10, 'a'))
+    A.affiche()
