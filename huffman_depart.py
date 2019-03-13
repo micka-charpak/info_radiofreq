713ea4911d4548f46067ccfa332d4e49300bb873
mickael renaud <mickael.renaud@u-psud.fr>
2019-03-13 12:16:56 +0100

    Update huffman_depart.py
    Travail sur la classe Huffman et pop_min()
    Le reste est fait ( encode_ascii() et frequences() )

--- huffman_depart.py
+++ huffman_depart.py
@@ -35,6 +35,36 @@ class Feuille(Arbre):
                 str(self.frequence) +
                 '(' + self.symbole + ')')
 
+
+def encode_ascii(texte):
+    '''
+    renvoie le codage ascii d'une chaîne de caractère
+    :param texte: str
+    :return: str de 0 et de 1 '10010100010110001...'
+    >>> encode_ascii('bonjour')
+    '01100010011011110110111001101010011011110111010101110010'
+    '''
+    texte_ascii=''
+    for car in texte :
+        texte_ascii += str(("{:08b}".format(ord(car))))
+    return texte_ascii
+
+def frequences(texte):
+    '''
+    renvoie un dictionnaire des caractères présents et de leur
+    fréquence    :param texte:
+    :return: dictionnaire de la forme {'A': 12, 'C': 3, ...}
+    >>> frequences('ABRACADABRA')
+    {'A': 5, 'B': 2, 'R': 2, 'C': 1, 'D': 1}
+    '''
+    l=[]
+    d={}
+    for i in range(len(texte)):
+        if texte[i] not in texte[0:i] :
+            d[texte[i]] = texte.count(texte[i])
+    return d
+
+
 class Huffman:
     """ Algorithme de construction de l'arbre de Huffman """
     def __init__(self, frequences):
@@ -43,8 +73,30 @@ class Huffman:
             frequences: dictionnaire des fréquences
         """
         self.foret = []
+        for lettre in frequences.keys():
+            self.foret.append(Feuille(frequences[lettre],lettre))
+    def pop_min(self):
+        """
+        Retire la feuille avec la plus petite fréquence de la forêt et la retourne
+        :return: Feuille
+        >>> Huffman(frequences('ABRACADABRA')).pop_min().frequence == 1
+        True
+        """
+        min = self.foret[0].frequence
+        for i in range(1,len(self.foret)):
+            if self.foret[i].frequence > self.foret[i-1].frequence:
+                min = self.foret[i]
+        return min
+        del self.foret
+
+
+
+
+
 
 if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
     A = Arbre(18,
               Arbre(8,
                     Arbre(3,
@@ -53,3 +105,5 @@ if __name__ == "__main__":
                     Feuille(5, 'b')),
               Feuille(10, 'a'))
     A.affiche()
+    print(frequences('ABRACADABRA'))
+
